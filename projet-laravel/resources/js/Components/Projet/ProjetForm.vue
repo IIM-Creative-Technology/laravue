251e23e3ff94294896bb723684a347e172bb749a
gauthe <eole.gauthe@edu.devinci.fr>
2021-03-27 22:48:15 +0100

    Form Projet terminé

--- projet-laravel/resources/js/Components/Projet/ProjetForm.vue
+++ projet-laravel/resources/js/Components/Projet/ProjetForm.vue
@@ -5,34 +5,21 @@
   <form @submit.prevent="submitForm">
     <label for="">Client</label>
     <input type="text" name="client" v-model="form.client" /><br />
-    <!-- <div v-if="form.errors.client">{{form.errors.client }}</div> -->
+     <div v-if="errors.client">{{errors.client }}</div>
     <label for="">Nom</label>
-    <input
-      type="text"
-      name="name_responsable"
-      v-model="form.name_responsable"
-    />
-    <br />
-    <div v-if="form.errors.name_responsable">
-      {{form.errors.name_responsable }}
-    </div>
+    <input type="text" name="name_responsable" v-model="form.name_responsable"/><br />
+    <div v-if="errors.name_responsable">{{errors.name_responsable }}</div>
     <label for="">Prénom</label>
-    <input
-      type="text"
-      name="prenom_responsable"
-      v-model="form.prenom_responsable"
-    /><br />
-    <div v-if="form.errors.prenom_responsable">
-      {{form.errors.prenom_responsable}}
-    </div>
+    <input type="text" name="prenom_responsable" v-model="form.prenom_responsable"/><br />
+    <div v-if="errors.prenom_responsable">{{errors.prenom_responsable}}</div>
     <label for="">Téléphone</label>
     <input
       type="tel"
       name="tel_responsable"
       v-model="form.tel_responsable"
     /><br />
-    <div v-if="form.errors.tel_responsable">
-      {{form.errors.tel_responsable }}
+    <div v-if="errors.tel_responsable">
+      {{errors.tel_responsable }}
     </div>
     <label for="">Email</label>
     <input
@@ -40,8 +27,8 @@
       name="email_responsable"
       v-model="form.email_responsable"
     /><br />
-    <div v-if="form.errors.email_responsable">
-      {{form.errors.email_responsable }}
+    <div v-if="errors.email_responsable">
+      {{errors.email_responsable }}
     </div>
 
     <h2>
@@ -51,27 +38,27 @@
 
     <label for="">Titre</label>
     <input type="text" name="title" v-model="form.title" /><br />
-    <div v-if="form.errors.title">{{ form.errors.title }}</div>
+    <div v-if="errors.title">{{ errors.title }}</div>
     <label for="">Description</label
     ><input type="text" name="description" v-model="form.description" /><br />
-    <div v-if="form.errors.description">{{ form.errors.description }}</div>
+    <div v-if="errors.description">{{ errors.description }}</div>
     <label for="">Début de projet</label
     ><input type="date" name="date_debut" v-model="form.date_debut" /><br />
-    <div v-if="form.errors.date_debut">{{ form.errors.date_debut }}</div>
+    <div v-if="errors.date_debut">{{ errors.date_debut }}</div>
     <label for="">Fin de projet estimé</label
     ><input type="date" name="date_fin" v-model="form.date_fin" /><br />
-    <div v-if="form.errors.date_fin">{{ form.errors.date_fin }}</div>
+    <div v-if="errors.date_fin">{{ errors.date_fin }}</div>
     <label for="">Status du projet</label
     ><input type="text" name="etat" v-model="form.etat" /><br />
-    <div v-if="form.errors.etat">{{ form.errors.etat }}</div>
+    <div v-if="errors.etat">{{ errors.etat }}</div>
     <label for="">Nombre de jours vendus </label
     ><input
       type="text"
       name="nombre_jours_vendus"
       v-model="form.nombre_jours_vendus"
     /><br />
-    <div v-if="form.errors.nombre_jours_vendus">
-      {{form.errors.nombre_jours_vendus }}
+    <div v-if="errors.nombre_jours_vendus">
+      {{errors.nombre_jours_vendus }}
     </div>
     <button type="submit">Annuler</button>
     <button type="submit">Sauvegarder</button>
@@ -80,7 +67,7 @@
 
 <script>
 export default {
-//   props: ["errors"],
+   props: ["errors"],
 
   data() {
     return {
